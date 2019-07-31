cc0acf8d3b3b9eeb628a7737554cebfc882c1443
MarcosCastelo <marcosvitorcastelo@gmail.com>
2019-07-31 17:34:32 -0300

    App reserva e seus modelos (Cliente, Filme, Sala, Poltrona e Sessa) criados e testados

---
+++ reserva/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from reserva.utils import *
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+
+
+class Filme(models.Model):
+    nome = models.CharField(max_length=50)
+    dublado = models.BooleanField
+    duracao = models.IntegerField
+    classificacao = models.IntegerField(choices=CLASSIFICACAO_INDICATIVA_CHOICES, default=1)
+    trailer = models.CharField(max_length=35)
+
+class Sala(models.Model):
+    nome = models.CharField(max_length=20)
+    tecnologia = models.IntegerField(choices=TECNOLOGIAS_CHOICES, default=1)
+
+class Poltrona(models.Model):
+    posicao = models.CharField(max_length=2)
+    tipo = models.IntegerField(choices=POLTRONAS_CHOICES, default=1)
+    cliente = models.ForeignKey(Cliente, related_name='poltrona_cliente', on_delete='CASCADE', null=True)
+    sala = models.ForeignKey(Sala, related_name='poltrona_sala', on_delete='CASCADE')
+
+class Sessao(models.Model):
+    data = models.DateField
+    horario = models.TimeField
+    sala = models.ForeignKey(Sala, related_name='sessao_sala', on_delete='CASCADE')
+    filme = models.ForeignKey(Filme, related_name='sessao_filme', on_delete='CASCADE')
---
+++ reserva/utils.py
@@ -0,0 +1,20 @@
+CLASSIFICACAO_INDICATIVA_CHOICES = (
+    (1, "L"),
+    (2, "10"),
+    (3, "12"),
+    (4, "14"),
+    (5, "16"),
+    (6, "18")
+)
+
+POLTRONAS_CHOICES = (
+    (1, "Simples"),
+    (2, "Namoradeira"),
+    (3, "Cadeirante"),
+    (4, "Acompanhante"),
+)
+
+TECNOLOGIAS_CHOICES = (
+    (1, '2D'),
+    (2, '3D'),
+)
