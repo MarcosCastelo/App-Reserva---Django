91c3a0f3f6f02c6131993513bd1bd55c08d4f430
MarcosCastelo <marcosvitorcastelo@gmail.com>
2019-08-02 03:51:58 -0300

    view index implementado, template index implementado, url index adicionada

--- reserva/views.py
+++ reserva/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, Http404
+from django.views import generic
+from .models import *
+from .utils import *
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'reserva/index.html'
+    context_object_name = 'sessao_list'
+
+    def get_queryset(self):
+        sessoes = list(Sessao.objects.filter(pub_data__range=(HOJE, FAIXA_DE_TEMPO)))
+        programacao = list()
+        for sessao in sessoes:
+            programacao.append(Sessao.objects.filter(filme = sessao.filme))
+            sessoes.remove(sessao)
+        return programacao
